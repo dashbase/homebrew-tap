04109c266c210ebb64427a59820d867dfb5e1bd3
Penghan Wang <peter@dashbase.io>
2019-04-30 19:13:24 +0800

    Brew formula update for night-watch version v1.1.1-rc1

--- Formula/night-watch.rb
+++ Formula/night-watch.rb
@@ -1,9 +1,9 @@
 class NightWatch < Formula
   desc "night-watch"
   homepage "https://dashbase.io/"
-  url "https://github.com/dashbase/night-watch/releases/download/v1.1.0/night-watch_1.1.0_macOS_64-bit.tar.gz"
-  version "1.1.0"
-  sha256 "80ddeb6dfb58ae3bc7239f0fed8d84212d9a3425adf9ea8fd282f58931bdd243"
+  url "https://github.com/dashbase/night-watch/releases/download/v1.1.1-rc1/night-watch_1.1.1-rc1_macOS_64-bit.tar.gz"
+  version "1.1.1-rc1"
+  sha256 "41a7d1392ceb2183be47dd956add2d38e61d5e0ccd9e9558cf71647c30df727f"
 
   def install
     bin.install "night-watch"
